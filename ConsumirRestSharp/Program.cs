8954daa34a8de4564abf69a90216b56efd466294
JoseMedina <JoseMedina@DESKTOP-J6GQEMM>
2023-05-08 19:25:42 +0200

    Agregar archivos de proyecto.

---
+++ ConsumirRestSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumirRestSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RestClient client = new RestClient("https://navision.furgotrayler.com/DynamicsNAV80USER/Odata/Company('Furgo-Trayler%20SL')/Proveedores?$filter=VAT_Registration_No%20eq%20%27A25221433%27");
+
+        }
+    }
+}
